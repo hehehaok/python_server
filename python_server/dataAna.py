beb81cf71c2b2dd97d29fd99615cf64e93540e43
hehehaok <278391728@qq.com>
2023-06-14 21:15:55 +0800

    三维定位v0.013

--- python_server/dataAna.py
+++ python_server/dataAna.py
@@ -26,18 +26,21 @@ class dataAna:
                 result[idx, :] = trilater.trilaterate()
         f.close()
         self.posResult = result
+        return result
 
     def calPosEKF(self):
         """
         静态条件二维下的EKF
+        状态方程： Xk = [[x_k],[y_k]] = [[1,0],[0,1]]@[[x_(k-1)],[y_(k-1)]]+[[wx_(k-1)],[wy_(k-1)]]
+        观测方程：4个基站到标签的距离作为观测量，参照论文[UWB Positioning System Based on LSTM Classification With Mitigated NLOS Effects]
         """
         # 初始化EKF
-        rk = ExtendedKalmanFilter(dim_x=2, dim_z=4)
-        rk.x = np.array(np.zeros((2,1)))
-        rk.F = np.eye(2)
+        rk = ExtendedKalmanFilter(dim_x=2, dim_z=4) # 状态向量的个数 观测向量的个数
+        rk.x = np.array(np.ones((2,1))) # 状态向量[[x_k],[y_k]]
+        rk.F = np.eye(2) # 状态转移矩阵
         rk.Q = np.diag([0.01, 0.01]) # 状态噪声
         rk.R = np.diag([0.01, 0.01, 0.01, 0.01]) # 观测噪声
-        rk.P *= 1
+        rk.P *= 0.1 # 均方误差阵
 
         f = open(self.filePath, "r")
         lines = f.readlines()
@@ -46,11 +49,12 @@ class dataAna:
             error_flag, result_dic = Process_String_Before_Udp(line)
             if error_flag == 0:
                 Info = BP_Process_String(result_dic)
-                rk.update(np.array(Info['distance']).T, calJacobia, calMeas)
-                result[idx, 0:2] = rk.x
+                rk.update(np.array(Info['distance'])[:, None], calJacobia, calMeas)
+                result[idx, 0:2] = rk.x.T
                 rk.predict()
         f.close()
         self.posResult = result
+        return result
 
     def calRMS2D(self):
         idx = np.where((self.posResult==np.array([0, 0, 0])).all(axis=1))[0]
@@ -61,51 +65,6 @@ class dataAna:
         idx = np.where((self.posResult==np.array([0, 0, 0])).all(axis=1))[0]
         posResult = np.delete(self.posResult, idx, axis=0)
         return np.mean(np.linalg.norm(posResult-self.truePos, axis=1))
-
-    def scatterPlot2D(self):
-        plt.figure(100)
-        plt.rcParams['font.family'] = ['SimHei']
-        p1 = plt.scatter(self.truePos[0,0], self.truePos[0,1], c='red', s=40)
-        p2 = plt.scatter(self.posResult[:,0], self.posResult[:,1], c='blue', s=30)
-        plt.legend([p1, p2], ['真实位置', '定位结果'])
-        plt.xlabel('x/米')
-        plt.ylabel('y/米')
-        plt.title('2维定位结果散点图')
-        plt.show()
-        return
-
-    def scatterPlot3D(self):
-        plt.figure(200)
-        ax = plt.axes(projection='3d')
-        plt.rcParams['font.family'] = ['SimHei']
-        p1 = ax.scatter3D(self.truePos[0,0], self.truePos[0,1], self.truePos[0,2], c='red', s=40)
-        p2 = ax.scatter3D(self.posResult[:,0], self.posResult[:,1], self.posResult[:,2], c='blue', s=30)
-        plt.legend([p1, p2], ['真实位置', '定位结果'])
-        plt.title('3维定位结果散点图')
-        plt.show()
-        return
-
-    def tracePlot(self, tracePoint):
-        plt.figure(300)
-        plt.rcParams['font.family'] = ['SimHei']
-        plt.plot(tracePoint[:, 0], tracePoint[:, 1])
-        plt.plot(self.posResult[:,0], self.posResult[:,1])
-        plt.legend(['真实轨迹', '定位轨迹'])
-        plt.title('2维定位结果轨迹图')
-        plt.show()
-        return
-
-    def scenePlot(self, tracePoint):
-        anchor = np.array([[0, 10.32, 10.32, 0, 0], [0, 0, 10.01, 10.01, 0]]).T
-        pillar = np.array([[1.78, 2.85, 2.85, 1.78, 1.78], [4.48, 4.48, 6.37, 6.37, 4.48]]).T
-        plt.figure(400)
-        plt.rcParams['font.family'] = ['SimHei']
-        plt.plot(tracePoint[:, 0], tracePoint[:, 1], '-')
-        plt.plot(anchor[:, 0], anchor[:, 1])
-        plt.plot(pillar[:, 0], pillar[:, 1])
-        plt.title('场地俯视图')
-        plt.show()
-        return
 
 def calJacobia(x):
     anchor = np.array([[0,0], [10.32, 0], [10.32, 10.01], [0, 10.01]])
@@ -122,18 +81,34 @@ def calMeas(x):
     pho = np.sqrt(xk ** 2 + yk ** 2)
     return pho[:, None]
 
-# 配置参数A1
-truePosA = np.array([[6.32, 3.2, 0]]) # 2维定位纵坐标设置为0
+# 场景坐标
+anchor = np.array([[0, 10.32, 10.32, 0, 0], [0, 0, 10.01, 10.01, 0]]).T # 基站坐标
+pillar = np.array([[1.78, 2.85, 2.85, 1.78, 1.78], [4.48, 4.48, 6.37, 6.37, 4.48]]).T # 柱坐标
+
+
+# 配置参数A1 - 二维静态
+truePosA1 = np.array([[6.32, 3.2, 0]]) # 2维定位纵坐标设置为0
 dir = 'logData/'
 log_file = 'A1.txt'
-filePathA = dir + log_file
+filePathA1 = dir + log_file
 
-dataA = dataAna(truePosA, filePathA)
-# dataA.calPos()
-dataA.calPosEKF()
-rmsA = dataA.calRMS2D()
-print(rmsA)
-dataA.scatterPlot2D()
+dataA1 = dataAna(truePosA1, filePathA1)
+posLS = dataA1.calPos()
+rmsLS = dataA1.calRMS2D()
+posEKF = dataA1.calPosEKF()
+rmsEKF = dataA1.calRMS2D()
+
+
+plt.figure(400)
+plt.rcParams['font.family'] = ['SimHei']
+p1 = plt.scatter(dataA1.truePos[0,0], dataA1.truePos[0,1], c='red', s=40)
+p2 = plt.scatter(posLS[:,0], posLS[:,1], c='blue', s=30)
+p3 = plt.scatter(posEKF[:,0], posEKF[:,1], c='green', s=30)
+plt.legend([p1, p2, p3], ['真实位置', 'LS结果%.4f'%(rmsLS), 'EKF结果%.4f'%(rmsEKF)], loc='lower right')
+plt.title('场地俯视图')
+plt.show()
+
+
 
 # 配置参数B5
 # truePosA = np.array([[6.32, 3.2, 0]]) # 2维定位纵坐标设置为0
